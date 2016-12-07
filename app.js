28b649bbf72ac811305ed9e98b156bc75885d329
verben-a <alina.verbenchuk@gmail.com>
2016-12-07 12:30:37 +0100

    Fiex styling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var Cocktail = function(taste, mood) {
     this.taste = taste;
     this.mood = mood;
@@ -16,60 +17,62 @@ function showMood() {
 }
 
 
-$(document).ready(function() {
+    $(document).ready(function() {
 
-    $('#content-taste p').on('click', function(e) {
-        e.preventDefault();
-        $('.taste-form').val($(this).text());
-        $('#content-taste').hide();
+        $('#content-taste p').on('click', function(e) {
+            e.preventDefault();
+            $('.taste-form').val($(this).text());
+            $('#content-taste').hide();
+        });
+
+        $('#content-mood p').on('click', function(e) {
+            e.preventDefault();
+            $('.mood-form').val($(this).text());
+            $('#content-mood').hide();
+        });
+
+        $('.submit-mix').on('click', function(e) {
+            e.preventDefault();
+
+            var taste = $('.taste-form').val();
+            var mood = $('.mood-form').val();
+
+            var pirateCocktail = new Cocktail(taste, mood);
+
+
+            if (taste == 'Strong') {
+                var strongDrink = ['Vodka', 'Whiskey'];
+                var strong = strongDrink[Math.floor(Math.random() * strongDrink.length)];
+                $('.ingredient').append(strong);
+
+            } else if (taste == 'Salty') {
+                var saltyDrink = ['Tequila', 'Gin'];
+                var salty = saltyDrink[Math.floor(Math.random() * saltyDrink.length)];
+                $('.ingredient').append(salty);
+
+            } else if (taste == 'Bitter') {
+                var bitterDrink = ['Campari', 'Grenadine'];
+                var bitter = bitterDrink[Math.floor(Math.random() * bitterDrink.length)];
+                $('.ingredient').append(bitter);
+
+            } else if (taste == 'Sweet') {
+                var sweetDrink = ['Vermouth', 'Cointreau'];
+                var sweet = sweetDrink[Math.floor(Math.random() * sweetDrink.length)];
+                $('.ingredient').append(bitter);
+
+            } else if (taste == 'Fruity') {
+                var fruityDrink = ['Cherry Liquer', 'Rum'];
+                var fruity = fruityDrink[Math.floor(Math.random() * fruityDrink.length)];
+                $('.ingredient').append(fruity);
+            }
+
+            var randomDrink = ['The Old Man and the Sea', 'Treasure Island', 'Moby-Dick', 'Twenty Thousand Leagues under the Sea', 'The Odyssey'];
+            var rand = randomDrink[Math.floor(Math.random() * randomDrink.length)];
+            console.log(rand);
+
+            $('.results').append(pirateCocktail.mixing(rand));
+            // alert('$('.results').append(pirateCocktail.mixing(rand))');
+
+        })
+
     });
-
-    $('#content-mood p').on('click', function(e) {
-        e.preventDefault();
-        $('.mood-form').val($(this).text());
-        $('#content-mood').hide();
-    });
-
-    $('.submit-mix').on('click', function(e) {
-        e.preventDefault();
-
-        var taste = $('.taste-form').val();
-        var mood = $('.mood-form').val();
-
-        var pirateCocktail = new Cocktail(taste, mood);
-
-        var randomDrink = ['The Old Man and the Sea', 'Treasure Island', 'Moby-Dick', 'Twenty Thousand Leagues under the Sea', 'The Odyssey'];
-        var strongDrink = ['Vodka', 'Whiskey'];
-        var saltyDrink = ['Tequila', 'Gin'];
-        var bitterDrink = ['Campari', 'Grenadine'];
-        var sweetDrink = ['Vermouth', 'Cointreau'];
-        var fruityDrink = ['Cherry Liquer', 'Rum'];
-
-        if (taste == 'Strong') {
-            var strong = strongDrink[Math.floor(Math.random() * strongDrink.length)];
-            $('.ingredient').append(strong);
-
-        } else if (taste == 'Salty') {
-            var salty = saltyDrink[Math.floor(Math.random() * saltyDrink.length)];
-            $('.ingredient').append(salty);
-
-        } else if (taste == 'Bitter') {
-            var bitter = bitterDrink[Math.floor(Math.random() * bitterDrink.length)];
-            $('.ingredient').append(bitter);
-
-        } else if (taste == 'Sweet') {
-            var sweet = sweetDrink[Math.floor(Math.random() * sweetDrink.length)];
-            $('.ingredient').append(bitter);
-
-        } else if (taste == 'Fruity') {
-            var fruity = fruityDrink[Math.floor(Math.random() * fruityDrink.length)];
-            $('.ingredient').append(fruity);
-        }
-
-        var rand = randomDrink[Math.floor(Math.random() * randomDrink.length)];
-
-        $('.results').append(pirateCocktail.mixing(rand));
-
-    })
-
-});
