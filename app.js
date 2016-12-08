0a8c3f964ed57494a0d5da3841d720f27f669be1
verben-a <alina.verbenchuk@gmail.com>
2016-12-08 10:03:10 +0100

    Fixed version with alert

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var Cocktail = function(taste, mood) {
     this.mood = mood;
 }
 
+
 Cocktail.prototype.mixing = function(rand) {
     return '<p>If you like ' + this.taste + ' drinks and have a ' + this.mood + ' mood you will like ' + rand + '</p>'
 }
 
+
 function showTaste() {
     $('.dropdown-content-taste').toggle('show');
 }
+
 
 function showMood() {
     $('.dropdown-content-mood').toggle('show');
@@ -18,11 +21,13 @@ function showMood() {
 
 $(document).ready(function() {
 
+
     $('#content-taste p').on('click', function(e) {
         e.preventDefault();
         $('.taste-form').val($(this).text());
         $('#content-taste').hide();
     });
+
 
     $('#content-mood p').on('click', function(e) {
         e.preventDefault();
@@ -30,46 +35,69 @@ $(document).ready(function() {
         $('#content-mood').hide();
     });
 
+
     $('.submit-mix').on('click', function(e) {
         e.preventDefault();
+
 
         var taste = $('.taste-form').val();
         var mood = $('.mood-form').val();
 
+
+        if (taste == '' && mood == '') {
+            alert('Choose the taste and mood!');
+            return
+        }
+
+
         var pirateCocktail = new Cocktail(taste, mood);
 
-        var randomDrink = ['The Old Man and the Sea', 'Treasure Island', 'Moby-Dick', 'Twenty Thousand Leagues under the Sea', 'The Odyssey'];
-        var strongDrink = ['Vodka', 'Whiskey'];
-        var saltyDrink = ['Tequila', 'Gin'];
-        var bitterDrink = ['Campari', 'Grenadine'];
-        var sweetDrink = ['Vermouth', 'Cointreau'];
-        var fruityDrink = ['Cherry Liquer', 'Rum'];
 
         if (taste == 'Strong') {
+            var strongDrink = ['Vodka', 'Whiskey'];
             var strong = strongDrink[Math.floor(Math.random() * strongDrink.length)];
+            console.log(strong);
             $('.ingredient').append(strong);
 
+
         } else if (taste == 'Salty') {
+            var saltyDrink = ['Tequila', 'Gin'];
             var salty = saltyDrink[Math.floor(Math.random() * saltyDrink.length)];
+            console.log(salty);
             $('.ingredient').append(salty);
 
+
         } else if (taste == 'Bitter') {
+            var bitterDrink = ['Campari', 'Grenadine'];
             var bitter = bitterDrink[Math.floor(Math.random() * bitterDrink.length)];
+            console.log(bitter);
             $('.ingredient').append(bitter);
+
 
         } else if (taste == 'Sweet') {
+            var sweetDrink = ['Vermouth', 'Cointreau'];
             var sweet = sweetDrink[Math.floor(Math.random() * sweetDrink.length)];
+            console.log(sweet);
             $('.ingredient').append(bitter);
 
+
         } else if (taste == 'Fruity') {
+            var fruityDrink = ['Cherry Liquer', 'Rum'];
             var fruity = fruityDrink[Math.floor(Math.random() * fruityDrink.length)];
+            console.log(fruity);
             $('.ingredient').append(fruity);
         }
 
+
+        var randomDrink = ['The Old Man and the Sea', 'Treasure Island', 'Moby-Dick', 'Twenty Thousand Leagues under the Sea', 'The Odyssey'];
         var rand = randomDrink[Math.floor(Math.random() * randomDrink.length)];
+        console.log(rand);
+
 
         $('.results').append(pirateCocktail.mixing(rand));
 
+
     })
+
 
 });
